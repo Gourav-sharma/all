b9152fa99f4c916c8ee95cd5c19f00be5bced5f9
Gourav-sharma <sharmagourav.2508@gmail.com>
2025-03-21 00:05:55 +0530

    update and refine for new version

--- lib/extensions/navigation_extension.dart
+++ lib/extensions/navigation_extension.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/material.dart';
 import 'package:all/all.dart';
 
 extension NavigationExtensions on BuildContext {
--- lib/global_context_initializer.dart
+++ lib/global_context_initializer.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/material.dart';
 import 'package:all/all.dart';
 
 class GlobalContextInitializer extends WidgetsBindingObserver {
